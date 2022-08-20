d474c1320d3b0eff959f23cf3781f1a4768cd8ad
Sebastian Kołodziej <sebastiankolodziej87@gmail.com>
2022-08-20 18:12:42 +0200

    finish task03 jsbasics

--- 03/app.js
+++ 03/app.js
@@ -7,10 +7,56 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+// getSum
 
+function getSum(a, b, c) {
+    const aInt = parseInt(a);
+    const bInt = parseInt(b);
+    const cInt = parseInt(c);
+   
+    const arr = [aInt, bInt, cInt];
+    arr.sort(function(a, b) {
+        return b - a;
+    });
 
+    return arr[0] + arr[1];
 
+}
 
+// const sum = getSum(a, b, c);
+// console.log(sum);
+
+// isEven
+
+const isEven = function(num) {
+    if(typeof num !== 'number') {
+        return null;
+    }
+    return num % 2 === 0;
+}
+
+// console.log(isEven(1), isEven(2), isEven('nanana'));
+
+// showInfo
+
+function showInfo(sum, even) {
+    switch(even) {
+        case null:
+            console.log('Podany argument '+ sum + ' nie jest liczbą');
+            break;
+        case true:
+            console.log('Podany argument '+ sum +' jest parzysty');
+            break;
+        case false:
+            console.log('Podany argument '+ sum +' jest nieparzysty');
+            break;
+    }
+}
+
+const sum = getSum(a, b, c);
+const even = isEven(sum);
+
+showInfo(sum, even);
 
 
 function randomNumber(min, max) {
